82bc62961469b39e15a3328971738a04db11f3a4
sabbir <samiubat14@gmail.com>
2021-04-14 12:15:54 +0600

    added setting page redirect to plugin admin page and color

--- Inc/Base/Setting.php
+++ Inc/Base/Setting.php
@@ -17,14 +17,26 @@ class Setting extends BaseController
     }
 
     public function settings_link( $links ) {  //4
-//        $settings_link = '<a href="admin.php?page=sabbir_plugin">SAM Settings</a>';  // for single use it this way
-       $settings_link = array(
+
+
+//     $settings_link = '<a href="admin.php?page=sabbir_plugin">SAM Settings</a>';  // for single use it this way
+
+       /*$settings_link = array(
            '<a href="admin.php?page=questionarie_based_filter_cqbf">Create Questionarie</a>',
            '<a href="admin.php?page=questionarie_based_filter">Settings</a>',
+
+        );*/
+
+        $settings_link = array(
+            sprintf("<a href='%s' style='color:#ff0000;' >%s</a>",admin_url('admin.php?page=questionarie_based_filter_cqbf'),__('Create Questionarie','questionarie_based_filter')),
+            sprintf("<a href='%s' style='color:#ff0000; font-weight: bold;' >%s</a>",admin_url('admin.php?page=questionarie_based_filter'),__('Settings','questionarie_based_filter'))
+
         );
+
 
 //        array_push( $links, $settings_link );   // for sinle use this
         return array_merge($links, $settings_link);
     }
+
 
 }
